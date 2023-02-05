5d675280864e54224637bae60925c5f05cf89a55
Angga Gantira <angga.gantira@gmail.com>
2023-02-05 10:47:47 +0700

    Flutter Tutorial Listtile & Listview separated Widget Android | iOS #7

--- lib/widgets/sample_listview.dart
+++ lib/widgets/sample_listview.dart
@@ -10,15 +10,24 @@ class SampleLietView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Belajar Widget List View'),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(20),
-        itemBuilder: (context, index) {
-          return Container(
-            color: Colors.purple[colorCodes[index]],
-            height: 100,
-          );
-        },
-        itemCount: colorCodes.length,
+      body: Container(
+        height: 130,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.all(10),
+          itemBuilder: (context, index) {
+            return CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.red,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage:
+                    NetworkImage('https://i.pravatar.cc/150?img=$index'),
+              ),
+            );
+          },
+          itemCount: colorCodes.length,
+        ),
       ),
     );
   }
